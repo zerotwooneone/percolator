98c9c511fbd77c4c6ea73c01cd718d74ad709388
zero <squiredd@gmail.com>
2024-07-26 15:50:33 -0700

    rsa encrypt/decrypt

--- source/Percolator.Console/Program.cs
+++ source/Percolator.Console/Program.cs
@@ -1,8 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Percolator.WindowsX.Api;
+using System.Security.Cryptography;
+using System.Text;
 
-var c = new Class1();
-c.Go();
+const string KeyContainerName = "Percolator";
 
-Console.WriteLine("Hello, World!");
+var csp = new CspParameters
+{
+    KeyContainerName = KeyContainerName,
+    Flags = 
+        CspProviderFlags.UseArchivableKey | 
+        CspProviderFlags.UseMachineKeyStore | 
+        CspProviderFlags.UseDefaultKeyContainer
+};
+
+byte[]? encrypted = null;
+//this gets or creates the key
+using (var rsa = new RSACryptoServiceProvider(csp))
+{
+    //Console.WriteLine($"rsa key: {rsa.ToXmlString(true)}");
+    encrypted = rsa.Encrypt("Hello World!"u8.ToArray(), false);
+}
+
+// using (var deleteRsa = new RSACryptoServiceProvider(csp))
+// {
+//     deleteRsa.PersistKeyInCsp = false;
+//     //this deletes the stored key, and disposes the object
+//     deleteRsa.Clear();
+//
+//     Console.WriteLine("Deleted key");
+// }
+
+using (var decrypter = new RSACryptoServiceProvider(csp))
+{
+    var decrypted = decrypter.Decrypt(encrypted, false);
+    Console.WriteLine(Encoding.UTF8.GetString(decrypted, 0, decrypted.Length));
+}
