d18cbcacceb77d839b49cf48713aca21e54c6061
zero <squiredd@gmail.com>
2024-07-26 19:25:53 -0700

    add stream test

--- source/Percolator.Grpc/Services/GreeterService.cs
+++ source/Percolator.Grpc/Services/GreeterService.cs
@@ -1,3 +1,6 @@
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Percolator.Grpc;
 
@@ -18,5 +21,24 @@ public class GreeterService : Greeter.GreeterBase
         {
             Message = "Hello " + request.Name
         });
+    }
+
+    public override Task Gimme(Empty request, IServerStreamWriter<StreamMessage> responseStream, ServerCallContext context)
+    {
+        using var readStream = File.OpenRead("gradient.bmp");
+        const int bufferSize = 1024;
+        var buffer = new byte[bufferSize];
+        int startIndex = 0;
+        while (readStream.Read(buffer, 0, buffer.Length) > 0)
+        {
+            var streamMessage = new StreamMessage
+            {
+                StartIndex = startIndex
+            };
+            streamMessage.Bytes.Add(ByteString.CopyFrom(buffer));
+            responseStream.WriteAsync(streamMessage);
+            startIndex += bufferSize;
+        }
+        return Task.CompletedTask;
     }
 }
