d27587935f2769d3a982e02760d139e5fee939de
zero <squiredd@gmail.com>
2024-07-26 19:56:50 -0700

    add client stream test

--- source/Percolator.Console/Program.cs
+++ source/Percolator.Console/Program.cs
@@ -2,6 +2,10 @@
 
 using System.Security.Cryptography;
 using System.Text;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Percolator.Console;
 
 const string KeyContainerName = "Percolator";
 
@@ -35,5 +39,35 @@ using (var decrypter = new RSACryptoServiceProvider(csp))
 {
     var decrypted = decrypter.Decrypt(encrypted, false);
     Console.WriteLine(Encoding.UTF8.GetString(decrypted, 0, decrypted.Length));
+}
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5076");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+var h = new Handler();
+h.Handle(client).Wait();
+
+Console.WriteLine("Press any key to exit...");
+
+public class Handler
+{
+    public async Task Handle(Greeter.GreeterClient client)
+    {
+        await using var writeFile = File.OpenWrite($"{DateTimeOffset.Now.ToUnixTimeMilliseconds()}.bmp");
+        var stream = client.Gimme(new Empty(), new CallOptions());
+
+        while (await stream.ResponseStream.MoveNext())
+        {
+            var streamMessage = stream.ResponseStream.Current;
+            //Console.WriteLine(streamMessage);
+            var bytes = streamMessage.Bytes.SelectMany(b=>b.ToByteArray()).ToArray();
+            writeFile.Position = streamMessage.StartIndex;
+            writeFile.Write(bytes, 0, bytes.Length);
+        }
+        await writeFile.FlushAsync();
+    }
 }
 
