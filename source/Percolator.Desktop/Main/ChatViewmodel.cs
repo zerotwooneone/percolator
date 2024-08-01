873a3e33891231c43d45e78e17d6d5d10984f441
zero <squiredd@gmail.com>
2024-08-01 14:59:08 -0700

    add chat layout

--- source/Percolator.Desktop/Main/ChatViewmodel.cs
+++ source/Percolator.Desktop/Main/ChatViewmodel.cs
@@ -1,6 +1,13 @@
+using System.Collections.ObjectModel;
+
 namespace Percolator.Desktop.Main;
 
 public class ChatViewmodel
+{
+    public ObservableCollection<MessageViewmodel> Messages { get; } = new();
+}
+
+public class MessageViewmodel
 {
     
 }
