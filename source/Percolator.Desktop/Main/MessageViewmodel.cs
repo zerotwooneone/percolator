c104d797839470b583a5f4ddf3f8d3f997a144b2
zero <squiredd@gmail.com>
2024-08-06 15:29:32 -0700

    set message background color

--- source/Percolator.Desktop/Main/MessageViewmodel.cs
+++ source/Percolator.Desktop/Main/MessageViewmodel.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Media;
 
 namespace Percolator.Desktop.Main;
 
@@ -9,9 +10,11 @@ public class MessageViewmodel
         Received = messageModel.Received.ToString("HH:mm:ss");
         Text = messageModel.Message;
         Alignment = messageModel.IsSelf ? TextAlignment.Right : TextAlignment.Left;
+        Background = messageModel.IsSelf ? new SolidColorBrush(Colors.LightBlue) : new SolidColorBrush(Colors.LightGreen);
     }
 
     public TextAlignment Alignment { get; }
+    public SolidColorBrush Background { get; } 
 
     public string Received { get; }
     public string Text { get; }
