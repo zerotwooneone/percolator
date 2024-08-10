899bdba8612597876ee17cfd71c84ed60addf92a
zero <squiredd@gmail.com>
2024-08-10 14:23:10 -0700

    obsolete onnext method

--- source/Percolator.Desktop/Main/IRemoteClientRepository.cs
+++ source/Percolator.Desktop/Main/IRemoteClientRepository.cs
@@ -8,7 +8,10 @@ public interface IRemoteClientRepository
     Observable<ByteString> ClientAdded { get; }
     RemoteClientModel? GetClientByIdentity(ByteString identity);
     RemoteClientModel GetOrAdd(ByteString identity, Func<ByteString, RemoteClientModel> addCallback);
+    
+    [Obsolete("remove this")]
     void OnNext(ByteString identity);
+    
     IDisposable WatchForChanges(RemoteClientModel remoteClient);
     IEnumerable<RemoteClientModel> GetAll();
 }
