e17e9ffd6d5c25d924420f17206a1a971895d441
zero <squiredd@gmail.com>
2024-08-11 14:36:02 -0700

    make view menu work

--- source/Percolator.Desktop/Main/MainWindowViewmodel.cs
+++ source/Percolator.Desktop/Main/MainWindowViewmodel.cs
@@ -5,11 +5,19 @@ namespace Percolator.Desktop.Main;
 public class MainWindowViewmodel
 {
     private readonly MainDefaultViewmodel _mainDefaultViewmodel;
+    private readonly SettingsViewmodel _settingsViewmodel;
     public BindableReactiveProperty<object?> SelectedViewmodel { get; } = new();
+    public BaseCommand SettingsCommand { get; }
+    public BaseCommand ChatCommand { get; }
 
-    public MainWindowViewmodel(MainDefaultViewmodel mainDefaultViewmodel)
+    public MainWindowViewmodel(
+        MainDefaultViewmodel mainDefaultViewmodel,
+        SettingsViewmodel settingsViewmodel)
     {
         _mainDefaultViewmodel = mainDefaultViewmodel;
+        _settingsViewmodel = settingsViewmodel;
         SelectedViewmodel.Value = _mainDefaultViewmodel;
+        SettingsCommand = new BaseCommand(_ => SelectedViewmodel.Value = _settingsViewmodel);
+        ChatCommand = new BaseCommand(_ => SelectedViewmodel.Value = _mainDefaultViewmodel);
     }
 }
