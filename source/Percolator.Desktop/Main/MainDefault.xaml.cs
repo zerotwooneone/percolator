ebc1c38d855b1feeb439c54f4ff2eca5a46f73a6
zero <squiredd@gmail.com>
2024-08-11 13:40:06 -0700

    pull maindefault into own control

---
+++ source/Percolator.Desktop/Main/MainDefault.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Percolator.Desktop.Main;
+
+public partial class MainDefault : UserControl
+{
+    public MainDefault()
+    {
+        InitializeComponent();
+    }
+}
