51adc93296a091218a4b226b8bb7863a544fb20e
zero <squiredd@gmail.com>
2025-06-29 16:04:00 -0700

    add repo methods

--- source/Percolator.Identity/IPeerRepository.cs
+++ source/Percolator.Identity/IPeerRepository.cs
@@ -4,4 +4,6 @@ public interface IPeerRepository
 {
     Task<Peer?> GetByIdAsync(PeerId peerId);
     Task<Peer?> GetByThumbprintAsync(string thumbprint);
+    Task AddAsync(Peer peer);
+    Task RemoveAsync(PeerId peerId);
 }
