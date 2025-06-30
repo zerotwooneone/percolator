84ce108ec51976c5633e04cdb76e164952028f7f
zero <squiredd@gmail.com>
2025-06-30 00:03:26 -0700

    tests pass

--- source/Percolator.Cryptography/CertificateGenerator.cs
+++ source/Percolator.Cryptography/CertificateGenerator.cs
@@ -6,8 +6,16 @@ namespace Percolator.Cryptography;
 
 public static class CertificateGenerator
 {
-    public static X509Certificate2 CreateSelfSignedCertificate(string commonName = "localhost")
+     public static X509Certificate2 CreateSelfSignedCertificate(string commonName = "localhost")
     {
+        // The key object must not be disposed, as the returned certificate
+        // object takes ownership of its lifetime.
+        var key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+        var request = new CertificateRequest(
+            $"CN={commonName}",
+            key,
+            HashAlgorithmName.SHA256);
+
         var sanBuilder = new SubjectAlternativeNameBuilder();
         sanBuilder.AddIpAddress(IPAddress.Loopback);
         sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
@@ -17,27 +25,27 @@ public static class CertificateGenerator
             sanBuilder.AddDnsName(commonName);
         }
 
-        using var rsa = RSA.Create(4096);
-        var request = new CertificateRequest(
-            $"cn={commonName}",
-            rsa,
-            HashAlgorithmName.SHA256,
-            RSASignaturePadding.Pkcs1);
+        request.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.KeyCertSign | X509KeyUsageFlags.DigitalSignature,
+            critical: true));
 
         request.CertificateExtensions.Add(sanBuilder.Build());
 
-        var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(1));
-
-        // We have to export and re-import the certificate to get the private key to be associated with it
-        // in a way that works across platforms. Using EphemeralKeySet is important for testability.
-        return new X509Certificate2(
-            certificate.Export(X509ContentType.Pfx),
-            (string?)null,
-            X509KeyStorageFlags.Exportable | X509KeyStorageFlags.EphemeralKeySet);
+        // CreateSelfSigned now correctly associates the private key.
+        // There is no need to call CopyWithPrivateKey.
+        return request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(1));
     }
 
     public static X509Certificate2 CreateTlsCertificate(string commonName = "localhost")
     {
+        // The key object must not be disposed, as the returned certificate
+        // object takes ownership of its lifetime.
+        var key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+        var request = new CertificateRequest(
+            $"CN={commonName}",
+            key,
+            HashAlgorithmName.SHA256);
+
         var sanBuilder = new SubjectAlternativeNameBuilder();
         sanBuilder.AddIpAddress(IPAddress.Loopback);
         sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
@@ -47,24 +55,14 @@ public static class CertificateGenerator
             sanBuilder.AddDnsName(commonName);
         }
 
-        using var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
-        var request = new CertificateRequest(
-            $"cn={commonName}",
-            ecdsa,
-            HashAlgorithmName.SHA256);
-
-        request.CertificateExtensions.Add(
-            new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, critical: true));
+        request.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.DigitalSignature,
+            critical: true));
 
         request.CertificateExtensions.Add(sanBuilder.Build());
 
-        var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(1));
-
-        // We have to export and re-import the certificate to get the private key to be associated with it
-        // in a way that works across platforms. Using EphemeralKeySet is important for testability.
-        return new X509Certificate2(
-            certificate.Export(X509ContentType.Pfx),
-            (string?)null,
-            X509KeyStorageFlags.Exportable | X509KeyStorageFlags.EphemeralKeySet);
+        // CreateSelfSigned now correctly associates the private key.
+        // There is no need to call CopyWithPrivateKey.
+        return request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(1));
     }
 }
