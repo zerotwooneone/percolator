8f94b9bc6f63038466f0034342c16acc61272d2d
zero <squiredd@gmail.com>
2025-07-02 18:57:43 -0400

    register crypto

--- source/Percolator.Application/Cryptography/ServiceCollectionExtensions.cs
+++ source/Percolator.Application/Cryptography/ServiceCollectionExtensions.cs
@@ -7,6 +7,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddCryptographyServices(this IServiceCollection services)
     {
+        services.AddSingleton<ISigningService, EcdsaSigningService>();
         services.AddSingleton<IX3DHManager,X3DHManager>();
         
         return services;
