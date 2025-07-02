b0a21c2f0aca15f0ccb1f2b2ce9c32c48b517723
zero <squiredd@gmail.com>
2025-07-02 19:24:54 -0400

    clean up identity

--- source/Percolator.Identity/IPeerIdentityStore.cs
+++
@@ -1,9 +0,0 @@
-namespace Percolator.Identity;
-
-public record PeerIdentity(byte[] IdentityKey, byte[] PreKeyBundle);
-
-public interface IPeerIdentityStore
-{
-    Task StorePeerAsync(PeerIdentity peer);
-    Task<PeerIdentity?> GetPeerAsync(byte[] identityKey);
-}
