d2007b0c147af44ce060c17184f705df1e81d9ac
zero <squiredd@gmail.com>
2025-07-12 00:13:41 -0700

    hack the one-time key generation

--- source/Percolator.Application/Identity/InMemoryOneTimeKeyProvider.cs
+++ source/Percolator.Application/Identity/InMemoryOneTimeKeyProvider.cs
@@ -1,4 +1,4 @@
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using Percolator.Identity;
 
@@ -6,24 +6,13 @@ namespace Percolator.Application.Identity;
 
 public class InMemoryOneTimeKeyProvider : IOneTimeKeyProvider
 {
-    // This would be populated at startup by a key management service.
-    private readonly ConcurrentQueue<ECDiffieHellman> _oneTimeKeys = new();
-
-    public ECDiffieHellman? PopOneTimeKey()
-    {
-        if (_oneTimeKeys.TryDequeue(out var key))
-        {
-            return key;
-        }
-        return null;
-    }
-
-    // Method to populate the queue, intended for use at application startup.
     public void AddKeys(IEnumerable<ECDiffieHellman> keys)
     {
-        foreach (var key in keys)
-        {
-            _oneTimeKeys.Enqueue(key);
-        }
+        // This is intentionally left empty as keys are generated on demand.
+    }
+
+    public ECDiffieHellman PopOneTimeKey()
+    {
+        return ECDiffieHellman.Create(ECCurve.NamedCurves.nistP256);
     }
 }
