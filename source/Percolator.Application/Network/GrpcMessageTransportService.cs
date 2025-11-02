ed516936753e9c4185c6584a9a1b09ffea1eb8a6
zero <squiredd@gmail.com>
2025-11-01 19:31:41 -0700

    remove comment

--- source/Percolator.Application/Network/GrpcMessageTransportService.cs
+++ source/Percolator.Application/Network/GrpcMessageTransportService.cs
@@ -57,8 +57,6 @@ public class GrpcMessageTransportService : IMessageTransportService
             throw new InvalidOperationException($"No gRPC endpoints found for peer {peer.Id}. Cannot send message.");
         }
 
-        //todo: use peerConnection.RelayPeerId to attempt a relay if direct send fails
-        
         //todo: loop over all the connections and try to send the message to all of them sequentially
         var endPoint = peerConnection.GrpcEndPoints[0];
 
